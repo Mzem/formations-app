93047434f08f9aa8eafd0138b4cb98195598e864
Malek Zemni <malek.zemni@octo.com>
2023-01-08 04:59:21 +0100

    add api key

--- api.js
+++ api.js
@@ -3,11 +3,20 @@ const app = express()
 
 const { formations } = require("./data.js")
 
+const API_KEY = "test"
+
 app.get("/", async function (req, res) {
   res.send("hello world!")
 })
 
 app.get("/formations", async function (req, res) {
+  const apiKeyFromHeader = req.headers["x-api-key"]
+
+  if (API_KEY !== apiKeyFromHeader) {
+    res.status(401).send({ code: 401, error: "FORBIDDEN" })
+    return
+  }
+
   const result = formations
   res.send(result)
 })
